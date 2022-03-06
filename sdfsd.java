504e972e4ef1c2701c093dfdcabfc503f462c8f8
bigfubiggui <76891109+bigfubiggui@users.noreply.github.com>
2022-03-06 21:11:54 +0800

    Create sdfsd.java

---
+++ sdfsd.java
@@ -0,0 +1,18 @@
+package com.atguigu.boot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * 这是一个springboot文件
+ */
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+
+    }
+}
